33f0cd1061977bf8dbfa0fe53116dde0ecc69675
marwa <marwajawad19@gmail.com>
2021-08-30 16:52:52 -0400

    remove extra / in the url

---
+++ src/Store/reducers/config.js
@@ -0,0 +1,7 @@
+export const key = '01947fdc028668cbba608f3d08618bef';
+export const api = 'https://api.themoviedb.org/3/';
+export const lang = 'en-US';
+export const page = '1';
+export const topRated = 'movie/top_rated';
+export const popular = 'movie/popular';
+export const upcoming = 'movie/upcoming';
