d52b2667eed7f4ff30688c1335bc2bf107f5b8f2
marwa <marwajawad19@gmail.com>
2021-08-30 16:51:11 -0400

    adding dispatch the fetch for top rated,upcoming and popular movies pls log the result

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  // fetchPopularMovies,
+  // fetchTopRatedMovies,
+  fetchUpComingMovies,
+} from './Store/reducers/UpComingReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  // const popularMovies = useSelector((state) => state);
+  const topRatedMovies = useSelector((state) => state);
+  console.log(topRatedMovies, 'top');
+  useEffect(() => {
+    dispatch(fetchUpComingMovies());
+    //   dispatch(fetchPopularMovies());
+  }, [dispatch]);
   return <div className="App">Hello React</div>;
 }
 
