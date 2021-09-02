ac7b5e4d3d9b40731b51fc108cd45e015215240f
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-09-02 23:29:14 +0300

    tailwind setup and styling Navbar

--- src/shared_components/Navbar.js
+++ src/shared_components/Navbar.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/movies">Movies</Link>
-      <Link to="/actors">Actors</Link>
-      <Link to="/collection">Collection</Link>
+    <nav className="flex w-3/4 justify-center items-end h-16 bg-white relative">
+      <div className="pr-8">
+        <NavLink
+          exact
+          className="p-8 hover:text-green-700"
+          activeClassName="text-green-600"
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="p-8 hover:text-green-700"
+          activeClassName="text-green-600"
+          to="/movies"
+        >
+          Movies
+        </NavLink>
+        <NavLink
+          className="p-8 hover:text-green-700"
+          activeClassName="text-green-600"
+          to="/actors"
+        >
+          Actors
+        </NavLink>
+      </div>
     </nav>
   );
 }
