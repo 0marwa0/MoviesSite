f2f720e573c4db8f76b903aa5a363d5feac179ab
Niyaz Irfan <niyaz.irfan119@gmail.com>
2021-09-02 18:39:29 +0300

    react router and routing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Footer from './shared_components/Footer';
+import Navbar from './shared_components/Navbar';
+import Sidebar from './shared_components/Sidebar';
+import HomePage from './components/homepage/HomePage';
+import Movies from './components/movies/Movies';
+import Actors from './components/actors/Actors';
 
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/collection" component={HomePage} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/shared_components/Navbar.js
+++ src/shared_components/Navbar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/movies">Movies</Link>
+      <Link to="/actors">Actors</Link>
+      <Link to="/collection">Collection</Link>
+    </nav>
+  );
 }
 
 export default Navbar;
