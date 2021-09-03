77c2e288cd10140cc3f287ab500aa3b504006143
marwa <marwajawad19@gmail.com>
2021-09-03 11:49:26 +0300

    adding popular, msot popular and sidbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  // fetchPopularMovies,
-  // fetchTopRatedMovies,
-  fetchUpComingMovies,
-} from './Store/reducers/UpComingReducer';
+import { fetchPopularMovies } from './Store/reducers/PopularReducer';
+import SideBar from './SideBar';
+import MostPopular from './MostPopular';
+import PopularCard from './PopularCard';
 
 function App() {
   const dispatch = useDispatch();
-  // const popularMovies = useSelector((state) => state);
-  const topRatedMovies = useSelector((state) => state);
-  console.log(topRatedMovies, 'top rated');
+  const popularMovies = useSelector(
+    (state) => state.popularMovies.data.results
+  );
+  const data = popularMovies ? popularMovies[10] : [];
   useEffect(() => {
-
-    dispatch(fetchUpComingMovies());
-    //   dispatch(fetchPopularMovies());
+    dispatch(fetchPopularMovies());
   }, [dispatch]);
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <SideBar />
+
+      <MostPopular data={data} />
+      <div>
+        <p className="WidgetTitle">Poupular Movies</p> <PopularCard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
