be72b2e54afabb44d61649dfa559773c915add81
marwa <marwajawad19@gmail.com>
2021-09-03 12:17:08 +0300

    solve conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchPopularMovies } from './Store/reducers/PopularReducer';
-import SideBar from './SideBar';
-import MostPopular from './MostPopular';
-import PopularCard from './PopularCard';
+import React from 'react';
 
 function App() {
-  const dispatch = useDispatch();
-  const popularMovies = useSelector(
-    (state) => state.popularMovies.data.results
-  );
-  const data = popularMovies ? popularMovies[10] : [];
-  useEffect(() => {
-    dispatch(fetchPopularMovies());
-  }, [dispatch]);
-  return (
-    <div className="App">
-      <SideBar />
-
-      <MostPopular data={data} />
-      <div>
-        <p className="WidgetTitle">Poupular Movies</p> <PopularCard />
-      </div>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
