1759d3a940c382c9427b07ef6f89d9c04ea07ba0
marwa <marwajawad19@gmail.com>
2021-09-03 12:03:34 +0300

    adding UpcomingCard componet to the sidebar

---
+++ src/SideBar/UserCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+function UserCard() {
+  return (
+    <div className="UserCard">
+      <img
+        src="https://i.picsum.photos/id/875/200/200.jpg?hmac=5faXLEO5BJEKazGrYKfm2NgT97z_7xtPutRkFkPO8Dk"
+        alt="profile "
+      />
+      <p>marwa</p>
+    </div>
+  );
+}
+
+export default UserCard;
