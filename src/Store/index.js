c29262bba2d54702b0327fac4ae824db891ad45f
marwa <marwajawad19@gmail.com>
2021-09-03 11:50:19 +0300

    adding genre reducer

--- src/Store/index.js
+++ src/Store/index.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import PopularovieReducer from './reducers/PopularReducer';
 import TopRatedReducer from './reducers/TopRatedReducer';
 import UpComingReducer from './reducers/UpComingReducer';
+import GenreReducer from './reducers/GenreReducer';
 
 const store = configureStore({
   reducer: {
     popularMovies: PopularovieReducer,
     topRatedMovies: TopRatedReducer,
     upComingMovies: UpComingReducer,
+    moviesGenre: GenreReducer,
   },
 });
 export default store;
