82cd5557d5600c0469b9192d2adf01d9599ff65f
marwa <marwajawad19@gmail.com>
2021-09-03 12:22:33 +0300

    return the app componet content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPopularMovies } from './Store/reducers/PopularReducer';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import SideBar from './SideBar';
+import 'antd/dist/antd.css';
+import MostPopular from './MostPopular';
+import PopularCard from './PopularCard';
 
 function App() {
-  return <div className="App" />;
+  const dispatch = useDispatch();
+  const popularMovies = useSelector(
+    (state) => state.popularMovies.data.results
+  );
+  const data = popularMovies ? popularMovies[0] : [];
+  useEffect(() => {
+    dispatch(fetchPopularMovies());
+  }, [dispatch]);
+  return (
+    <div className="App">
+      <SideBar />
+
+      <MostPopular data={data} />
+      <div>
+        <p className="WidgetTitle">Poupular Movies</p> <PopularCard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
