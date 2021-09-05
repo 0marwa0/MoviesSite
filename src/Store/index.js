f72ce5044fabe36e9f0e6e22ada7de6369bf4123
marwa <marwajawad19@gmail.com>
2021-09-05 23:26:55 +0300

    adding new reducers

--- src/Store/index.js
+++ src/Store/index.js
@@ -4,6 +4,12 @@ import PopularovieReducer from './reducers/PopularReducer';
 import TopRatedReducer from './reducers/TopRatedReducer';
 import UpComingReducer from './reducers/UpComingReducer';
 import GenreReducer from './reducers/GenreReducer';
+import PopularPeopleReducer from './reducers/PopularPeopleReducer';
+import MovieReducer from './reducers/MovieReducer';
+import PersonReducer from './reducers/PersonReducer';
+import SimilarMoviesReducer from './reducers/SimilarMoviesReducer';
+import VideoReducer from './reducers/VideoReducer';
+import MovieCast from './reducers/MovieCast';
 
 const store = configureStore({
   reducer: {
@@ -11,6 +17,12 @@ const store = configureStore({
     topRatedMovies: TopRatedReducer,
     upComingMovies: UpComingReducer,
     moviesGenre: GenreReducer,
+    popularPeople: PopularPeopleReducer,
+    person: PersonReducer,
+    movie: MovieReducer,
+    simialrMovies: SimilarMoviesReducer,
+    video: VideoReducer,
+    movieCast: MovieCast,
   },
 });
 export default store;
